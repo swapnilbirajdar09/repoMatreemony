e447579dd5987b440721237e4684d3de06977754
swapnilbirajdar09 <33017986+swapnilbirajdar09@users.noreply.github.com>
2018-10-08 17:48:16 +0530

    commit

---
+++ application/controllers/admin/Admin_profile.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_profile extends CI_Controller {
+
+    // Login controller
+    public function __construct() {
+        parent::__construct();
+        // load common model
+        $this->load->model('admin/Adminlogin_model');
+    }
+
+    // main index function
+    public function index() {
+        // start session		
+        $admin_name = $this->session->userdata('admin_name'); //----session variable
+        if ($admin_name == '') {
+            redirect('admin/admin_login');
+        }
+        $this->load->view('includes/adminheader');
+        $this->load->view('pages/admin/'); //------loading the admin login view
+    }
+
+}
