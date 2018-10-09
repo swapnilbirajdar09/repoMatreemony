1cecf5de9642eb66a5fd2023b43d041899bff650
swapnilbirajdar09 <33017986+swapnilbirajdar09@users.noreply.github.com>
2018-10-09 12:25:33 +0530

    swap commit

--- application/views/includes/adminheader.php
+++ application/views/includes/adminheader.php
@@ -1,60 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    Parinay Vadhu-Var Suchak Mandal
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="<?php echo base_url(); ?>assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="<?php echo base_url(); ?>assets/demo/demo.css" rel="stylesheet" />
-   <script src="<?php echo base_url(); ?>assets/js/const.js"></script>
-</head>
-
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="azure" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
-      <!--
-        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
-
-        Tip 2: you can also add an image using data-image tag
-    -->
-      <div class="logo">
-       <a href="#"><img src="<?php echo base_url(); ?>assets/images/wedding-hand.jpg" alt="" style="margin-left: 30px;" /></a>
-       
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item active  ">
-            <a class="nav-link" href="<?php echo base_url(); ?>admin/admin_dashboard">
-              <i class="material-icons">dashboard</i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="<?php echo base_url(); ?>admin/admin_profile">
-              <i class="material-icons">person</i>
-              <p>User Profile</p>
-            </a>
-          </li>
-         
-        
-        
-          <!-- <li class="nav-item active-pro ">
-                <a class="nav-link" href="./upgrade.html">
-                    <i class="material-icons">unarchive</i>
-                    <p>Upgrade to PRO</p>
-                </a>
-            </li> -->
-        </ul>
-      </div>
-    </div>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>
+            Parinay Vadhu-Var Suchak Mandal
+        </title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <!-- CSS Files -->
+        <link href="<?php echo base_url(); ?>assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+        <!-- CSS Just for demo purpose, don't include it in your project -->
+        <link href="<?php echo base_url(); ?>assets/demo/demo.css" rel="stylesheet" />
+        <script src="<?php echo base_url(); ?>assets/js/const.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/angular.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/angular-sanitize.js"></script>
+    </head>
+    <body class="">
+        <div class="wrapper ">
+            <div class="sidebar" data-color="azure" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+                <!--
+                  Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"          
+                  Tip 2: you can also add an image using data-image tag
+                -->
+                <div class="logo">
+                    <a href="#"><img src="<?php echo base_url(); ?>assets/images/wedding-hand.jpg" alt="" style="margin-left: 30px;" /></a>
+                </div>
+                <div class="sidebar-wrapper">
+                    <ul class="nav">
+                        <li class="nav-item active  ">
+                            <a class="nav-link" href="<?php echo base_url(); ?>admin/admin_dashboard">
+                                <i class="material-icons">dashboard</i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="<?php echo base_url(); ?>admin/admin_profile">
+                                <i class="material-icons">person</i>
+                                <p>User Profile</p>
+                            </a>
+                        </li>
+                        <!-- <li class="nav-item active-pro ">
+                              <a class="nav-link" href="./upgrade.html">
+                                  <i class="material-icons">unarchive</i>
+                                  <p>Upgrade to PRO</p>
+                              </a>
+                          </li> -->
+                    </ul>
+                </div>
+            </div>
