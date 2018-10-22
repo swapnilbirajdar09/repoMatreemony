a230908228d01bdc00a53779d9ba6e23af31cd93
swapnilbirajdar09 <33017986+swapnilbirajdar09@users.noreply.github.com>
2018-10-22 10:51:37 +0530

    commit by swap

--- application/controllers/admin/Admin_profile.php
+++ application/controllers/admin/Admin_profile.php
@@ -30,39 +30,45 @@ class Admin_profile extends CI_Controller {
         extract($_FILES);
         $data = $_POST;
 
-        $allowed_types=['gif','jpg','png','jpeg','JPG','GIF','JPEG','PNG'];
-       $imagePath ='';
-  if(!empty(($_FILES['profile_image']['name']))){
-    $extension = pathinfo($_FILES['profile_image']['name'], PATHINFO_EXTENSION);
+        $allowed_types = ['gif', 'jpg', 'png', 'jpeg', 'JPG', 'GIF', 'JPEG', 'PNG'];
+        $imagePath = '';
 
-    $_FILES['userFile']['name'] = 'userDashImage.'.$extension;
-    $_FILES['userFile']['type'] = $_FILES['profile_image']['type'];
-    $_FILES['userFile']['tmp_name'] = $_FILES['profile_image']['tmp_name'];
-    $_FILES['userFile']['error'] = $_FILES['profile_image']['error'];
-    $_FILES['userFile']['size'] = $_FILES['profile_image']['size'];
+        $image_name = $_FILES['profile_image']['name'];
+        if ($profile_imageEdit == '') {
+            $imagePath = '';
+        } else {
+            $imagePath = $profile_imageEdit;
+        }
 
-      $uploadPath ='assets/images/admin/';  //upload images in images/desktop/ folder
-      $config['upload_path'] = $uploadPath;
-      $config['allowed_types'] = 'gif|jpg|png|jpeg'; //allowed types of images           
-      $config['overwrite'] = TRUE;            
-     // print_r($fileData = $this->upload->data());die();
-      $this->load->library('upload', $config);  //load upload file config.
-      $this->upload->initialize($config);
+        if (!empty(($_FILES['profile_image']['name']))) {
+            $extension = pathinfo($_FILES['profile_image']['name'], PATHINFO_EXTENSION);
 
-      if($this->upload->do_upload('userFile')){
-        $fileData = $this->upload->data();
-        $imagePath = $uploadPath.$fileData['file_name'];
+            $_FILES['userFile']['name'] = 'adminImage.' . $extension;
+            $_FILES['userFile']['type'] = $_FILES['profile_image']['type'];
+            $_FILES['userFile']['tmp_name'] = $_FILES['profile_image']['tmp_name'];
+            $_FILES['userFile']['error'] = $_FILES['profile_image']['error'];
+            $_FILES['userFile']['size'] = $_FILES['profile_image']['size'];
 
+            $uploadPath = 'assets/images/admin/';  //upload images in images/desktop/ folder
+            $config['upload_path'] = $uploadPath;
+            $config['allowed_types'] = 'gif|jpg|png|jpeg'; //allowed types of images           
+            $config['overwrite'] = TRUE;
+            // print_r($fileData = $this->upload->data());die();
+            $this->load->library('upload', $config);  //load upload file config.
+            $this->upload->initialize($config);
 
-      }
-    }
-    
-   //echo $imagePath;die();
-  //validating image ends---------------------------//
-  //print_r($data);die();
-    $data['imagePath']=$imagePath;
-     // print_r($data);die();
+            if ($this->upload->do_upload('userFile')) {
+                $fileData = $this->upload->data();
+                $imagePath = $uploadPath . $fileData['file_name'];
+            }
+        }
 
+        //echo $imagePath;die();
+        //validating image ends---------------------------//
+        //print_r($data);die();
+        $data['imagePath'] = $imagePath;
+//        print_r($data);
+//        die();
         // call to model function to update AdminDetails
         $result = $this->Adminprofile_model->updateAdminDetails($data);
         if ($result) {
