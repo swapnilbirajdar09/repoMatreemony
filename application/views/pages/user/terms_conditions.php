4c578e37a336c8b61db4ccdb9bcbea6684e175ab
swapnilbirajdar09 <33017986+swapnilbirajdar09@users.noreply.github.com>
2018-11-28 16:49:08 +0530

    commit by swap

--- application/views/pages/user/terms_conditions.php
+++ application/views/pages/user/terms_conditions.php
@@ -1,7 +1,7 @@
 
 <!-- About section -->
 <div class="w3-container  w3-center  w3-padding-32" id="about" style="margin-top: 80px">
-    <div class="w3-content w3-justify" style="max-width:600px">
+    <div class="w3-content w3-justify" style="max-width:1000px">
         <div class="w3-col l12 w3-center w3-margin-top w3-margin-bottom w3-border-top w3-border-bottom">
             <h4>TERMS AND CONDITIONS</h4>
         </div>
@@ -20,72 +20,80 @@
             </p>
             <p>This is a legal agreement between you and Buddhist Parinay Matrimony. 
                 By using this <u>www.buddhistparinay.in</u> you agree to the following Terms and Conditions.</p>
+            <p> 1. With due respect, please fill in all the information correctly.so that you can stay in faith in the future life of the future.</p>
+            <p> 2. It is necessary to make sure that the information obtained in the indicator center on the resultant view is true.</p>
+            <p> 3. The bride and groom will get information on the relevant places required after paying the money and registering it.</p>
+            <p> 4. The ad will not be published without your permission after registration of the bride and groom.</p>
+            <p> 5. The fee given once will not be refunded.</p>
+            <p> 6. If any information is wrong then parents will be responsible for that.</p>
         </div>
     </div>
-    <!--    <div class="w3-content w3-justify" style="max-width:600px">
-            <div class="w3-col l12">
-                <div class="w3-col l2 s2">
-                    <span class="w3-xxlarge fa fa-camera"></span>
+    <div class="w3-content w3-justify" style="max-width:1000px">
+        <div class="w3-col l12">
+            <div class="w3-col l2 s2">
+                <span class="w3-xxlarge fa fa-camera"></span>
+            </div>
+            <div class="w3-col l10 s10 w3-left">
+                <div class="w3-col l12 w3-padding-small">
+                    <label class="w3-medium">Click Photo !</label>
                 </div>
-                <div class="w3-col l10 s10 w3-left">
-                    <div class="w3-col l12 w3-padding-small">
-                        <label class="w3-medium">Click Photo !</label>
-                    </div>
-                    <div class="w3-col l12 w3-padding-small">
-                        <p>Upload your Image. We need to open your system's camera to get your image for profile.</p>
-                        <p>Personal Data:  Access Permission of Mobile's Camera.</p>
-                    </div>
-    
+                <div class="w3-col l12 w3-padding-small">
+                    <p>Upload your Image. We need to open your system's camera to get your image for profile.</p>
+                    <p>Personal Data:  Access Permission of Mobile's Camera.</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="w3-col l12">
+            <div class="w3-col l2 s2">
+                <span class="w3-xxlarge fa fa-inbox"></span>
+            </div>
+            <div class="w3-col l10 s10 w3-left">
+                <div class="w3-col l12 w3-padding-small">
+                    <label class="w3-medium">Authenticate yourself.</label>
+                </div>
+                <div class="w3-col l12 w3-padding-small">
+                    <p class="">Verify your Email-Id registered. We verify your Email-Id by sending a link on your registered mail and then you can click on that link to check the registered email verification Successful.</p>
+                    <p>Personal Data:  Email Id.</p>
                 </div>
             </div>
-            <div class="w3-col l12">
-                <div class="w3-col l2 s2">
-                    <span class="w3-xxlarge fa fa-inbox"></span>
+        </div>
+        <div class="w3-col l12">
+            <div class="w3-col l2 s2">
+                <span class="w3-xxxlarge fa fa-mobile"></span>
+            </div>
+            <div class="w3-col l10 s10 w3-left">
+                <div class="w3-col l12 w3-padding-small">
+                    <label class="w3-medium">Authenticate yourself.</label>
                 </div>
-                <div class="w3-col l10 s10 w3-left">
-                    <div class="w3-col l12 w3-padding-small">
-                        <label class="w3-medium">Authenticate yourself.</label>
-                    </div>
-                    <div class="w3-col l12 w3-padding-small">
-                        <p class="">Verify your Email-Id registered. We verify your Email-Id by sending a link on your registered mail and then you can click on that link to check the registered email verification Successful.</p>
-                        <p>Personal Data:  Email Id.</p>
-                    </div>
+                <div class="w3-col l12 w3-padding-small">
+                    <p class="">Verify your Mobile registered. We verify your Mobile No by sending an OTP code over it and then you can get verified by using that code.
+                        We May Use The Information To Customize The Website/App According To Your Interest. To Contact You Via Email, Facsimile, Phone OR Text Message, To Deliver Certain Services Or Information You Have Requested.
+                    </p>
+                    <p>Personal Data:  Mobile No.</p>
                 </div>
             </div>
-            <div class="w3-col l12">
-                <div class="w3-col l2 s2">
-                    <span class="w3-xxxlarge fa fa-mobile"></span>
+        </div>
+        <div class="w3-col l12">
+            <div class="w3-col l2 s2">
+                <span class="w3-xxlarge fa fa-id-card"></span>
+            </div>
+            <div class="w3-col l10 s10 w3-left">
+                <div class="w3-col l12 w3-padding-small">
+                    <label class="w3-medium">Document Verification.</label>
                 </div>
-                <div class="w3-col l10 s10 w3-left">
-                    <div class="w3-col l12 w3-padding-small">
-                        <label class="w3-medium">Authenticate yourself.</label>
-                    </div>
-                    <div class="w3-col l12 w3-padding-small">
-                        <p class="">Verify your Mobile registered. We verify your Mobile No by sending an OTP code over it and then you can get verified by using that code.</p>
-                        <p>Personal Data:  Mobile No.</p>
-                    </div>
+                <div class="w3-col l12 w3-padding-small">
+                    <p class="">Verify your Uploaded Documents for Ex Aadhar Card, Pan Card, Driving license etc. 
+                    <p>Personal Data:  Images of Aadhar Card, Pan Card, Driving license ETC.</p>
                 </div>
             </div>
-            <div class="w3-col l12">
-                <div class="w3-col l2 s2">
-                    <span class="w3-xxlarge fa fa-id-card"></span>
-                </div>
-                <div class="w3-col l10 s10 w3-left">
-                    <div class="w3-col l12 w3-padding-small">
-                        <label class="w3-medium">Document Verification.</label>
-                    </div>
-                    <div class="w3-col l12 w3-padding-small">
-                        <p class="">Verify your Uploaded Documents for Ex Aadhar Card, Pan Card, Driving license etc. 
-                        <p>Personal Data:  Images of Aadhar Card, Pan Card, Driving license ETC.</p>
-                    </div>
-                </div>
-            </div>
-            <p class="w3-margin-top">
-                <strong><i class="fa fa-phone-square w3-large"></i> Contact Us</strong>
-            </p>
-            <p>If you have any questions or suggestions about our Privacy Policy and have any query about our Services, do not hesitate to contact
-                us on <a href="mailto:customercare@jumlakuwait.com" class="w3-text-blue">customercare@jumlakuwait.com</a>.
-            </p> 
-        </div>-->
+        </div>
+        <p class="w3-margin-top">
+            <strong><i class="fa fa-phone-square w3-large"></i> Contact Us</strong>
+        </p>
+        <p>If you have any questions or suggestions about our Privacy Policy and have any query about our Services, do not hesitate to contact
+            us on <a href="mailto:support@buddhistparinay.in" class="w3-text-blue">support@buddhistparinay.in</a>.
+        </p> 
+    </div>
 </div>
 
